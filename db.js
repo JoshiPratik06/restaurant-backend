8e5af005888bf1a4655e5e8542c67c32e1c6250d
Pratik JOshi <pratikjoshi0068@gmail.com>
2026-04-14 00:32:13 +0530

    fix db connection

--- db.js
+++ db.js
@@ -1,12 +1,17 @@
 const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST ,
-  user: process.env.DB_USER ,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-console.log("✅ MySQL Pool Ready");
+console.log("✅ MySQL Pool Ready (Cloud)");
 
 module.exports = db;
